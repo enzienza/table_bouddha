8f6502ce31966816f55351452c32bc4269772482
EnzaEnzi <enza.webdev@gmail.com>
2020-07-15 15:54:04 +0200

    modif bg radial-gradient

--- inc/customize/custom-dashboard.php
+++ inc/customize/custom-dashboard.php
@@ -21,14 +21,14 @@
  */
 function remove_menus(){
   // remove_menu_page('index.php');                  // Tableau de bord
-  remove_menu_page('edit.php');                   // Articles
+  remove_menu_page('edit.php');                      // Articles
   // remove_menu_page('upload.php');                 // Media
-  remove_menu_page('edit.php?post_type=page');    // Pages
-  remove_menu_page('edit-comments.php');          // Commentaires
+  remove_menu_page('edit.php?post_type=page');       // Pages
+  remove_menu_page('edit-comments.php');             // Commentaires
   // remove_menu_page('themes.php');                 // Apparences
   // remove_menu_page('plugins.php');                // Extentions
   // remove_menu_page('users.php');                  // Utilisateurs
-  remove_menu_page('tools.php');                  // Outils
+  remove_menu_page('tools.php');                     // Outils
   // remove_menu_page('options-general.php');        // Réglages
 
   // remove_menu_page('edit.php?post_type=recettes'); // pour masque un custom_post_type
